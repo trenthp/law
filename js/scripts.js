8103a2bc5c19cb85f283746d57cfdc3afe03c2d2
Trent Petersen <Foxtrot@Foxtrot.local>
2014-04-13 15:26:58 -0700

    map added

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,14 @@ $(document).foundation();
 
 $(document).ready(function(){
 
+	new Maplace({
+		show_markers: true,
+		locations: [{
+			lat: 45.6297529, 
+			lon: -122.6718371,
+			zoom: 16
+		}]
+	}).Load();
 
 	console.log('Page Loaded. Lets Do this!');
 
