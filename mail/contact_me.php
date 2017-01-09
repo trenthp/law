ca7d104062e157bac75dec85d97490c89d459f69
Trent Petersen <trentpetersen@gmail.com>
2017-01-09 15:56:13 -0800

    contact
    contact email update and spacing issues in contact section

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'trent@redtimberdesign.com';
+$to = 'office@petersenlawgroup.com';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@redtimberdesign.com\n"; 
