813c5aebc0aec48e753f09f74105664149e6e9ae
Trent <trent@toris-mbp.lan>
2019-06-29 09:20:20 -0600

    real update this time
    last was fake. Forgetting where hosted files actually are can be tough at time

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,7 +19,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'office@petersenlawgroup.com';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@petersenlawgroup.com\n"; 
+$headers = "From: noreply@redtimberdesign.com\n"; 
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
